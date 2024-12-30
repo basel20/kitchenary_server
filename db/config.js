7069ccc6b7636d848a8c8ad22d741e81366de749
basel20 <67703753+basel20@users.noreply.github.com>
2024-12-30 16:55:57 +0300

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectDB = async (url) => {
-    try {
-        mongoose.set('strictQuery', true); // Deprecation warning fix
-        await mongoose.connect(url, {
-            useNewUrlParser: true,        // Recommended settings
-            useUnifiedTopology: true,    // Recommended settings
-        });
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1); // Exit process with failure
-    }
-};
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true);
 
-module.exports = connectDB;
+    mongoose.connect(url)
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err));
+}
+
+module.exports = connectDB
